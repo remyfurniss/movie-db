52a96b3d2cfc9afd3e50ce2e06f35bda2632408d
Remy <remy.furniss@gmail.com>
2026-02-27 08:14:15 +0900

    simplified protected layout

--- frontend/src/layouts/ProtectedLayout.tsx
+++ frontend/src/layouts/ProtectedLayout.tsx
@@ -1,35 +1,10 @@
 import { Outlet } from "react-router-dom";
-import { useState, useEffect } from "react";
 import TopBar from "../components/TopBar";
-import { searchTmdbMovies } from "../api/api";
-import type { Movie } from "../types/movie";
 
 export default function ProtectedLayout() {
-  const [searchValue, setSearchValue] = useState("");
-  const [searchResults, setSearchResults] = useState<Movie[]>([]);
-
-  useEffect(() => {
-    if (!searchValue.trim()) {
-      setSearchResults([]);
-      return;
-    }
-
-    const timeout = setTimeout(async () => {
-      const data = await searchTmdbMovies(searchValue);
-      setSearchResults(data);
-    }, 300);
-
-    return () => clearTimeout(timeout);
-  }, [searchValue]);
-
   return (
     <>
-      <TopBar
-        searchValue={searchValue}
-        onSearchChange={setSearchValue}
-        results={searchResults}
-        onSelectMovie={() => setSearchValue("")}
-      />
+      <TopBar/>
       <Outlet />
     </>
   );
