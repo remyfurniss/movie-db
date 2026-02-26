f7d74d0bb999a456e1000c4eae3bb5965cf9828d
Remy <remy.furniss@gmail.com>
2026-02-27 08:11:03 +0900

    cleaned up App.tsx 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,17 @@
 import './App.css'
-import { Routes, Route, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-
-import {
-  fetchWatchlists,
-  createWatchlist,
-  addMovieToWatchlist,
-  searchTmdbMovies
-} from "./api/api";
+import { Routes, Route } from "react-router-dom";
 
 import { useAuth, AuthProvider } from "./context/authContext";
 import { WatchlistProvider } from "./context/watchlistContext";
 
+import TopBar from './components/TopBar'; 
 import ProtectedRoute from "./components/ProtectedRoute";
+
 import ProtectedLayout from "./layouts/ProtectedLayout";
 import PublicLayout from "./layouts/PublicLayout";
 
 import Home from './pages/Home'; 
 import MovieDetail from "./pages/MovieDetail";
-import TopBar from './components/TopBar'; 
 import WatchlistDetail from './pages/WatchlistDetail';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -27,6 +20,7 @@ function AppRoutes() {
 
   return (
     <Routes>
+
       {/* public */}
       <Route element={<PublicLayout />}>
         <Route path="/login" element={<Login />} />
@@ -34,45 +28,39 @@ function AppRoutes() {
       </Route>
 
       {/* protected */}
-      <Route
-  element={
-    <ProtectedRoute>
-      <WatchlistProvider>
-        <ProtectedLayout />
-      </WatchlistProvider>
-    </ProtectedRoute>
-  }
->
+      <Route element={
+        <ProtectedRoute>
+          <WatchlistProvider>
+            <ProtectedLayout />
+          </WatchlistProvider>
+        </ProtectedRoute>}>
         <Route path="/" element={<Home />} />
         <Route path="/movies/:id" element={<MovieDetail />} />
         <Route path="/movies/tmdb/:tmdbId" element={<MovieDetail />} />
         <Route path="/watchlists/:id" element={<WatchlistDetail />} />
       </Route>
+
     </Routes>
   );
 }
 
-function AppWrapper() {
-  return (
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  );
-}
-
-function App() {
-
+function InnerApp() {
   const { user } = useAuth();
 
   return (
     <div className='app'>
-      {user && (  // only render TopBar if logged in
-    <TopBar/>
-  )}
-      <AppRoutes/>
+      {user && <TopBar />}
+      <AppRoutes />
     </div>
   );
 }
 
-export default AppWrapper;
-//export default App
+function App() {
+  return (
+    <AuthProvider>
+      <InnerApp />
+    </AuthProvider>
+  );
+}
+
+export default App;
